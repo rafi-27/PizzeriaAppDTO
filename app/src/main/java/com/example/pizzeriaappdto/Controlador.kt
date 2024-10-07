86d28e4aa49a9d366f8bcfec73a08d3f506fa549
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-10-07 23:16:56 +0200

    ya

--- app/src/main/java/com/example/pizzeriaappdto/Controlador.kt
+++ app/src/main/java/com/example/pizzeriaappdto/Controlador.kt
@@ -1,6 +1,7 @@
 package com.example.pizzeriaappdto
 
 import android.os.Build
+import android.view.PixelCopy
 import androidx.annotation.RequiresApi
 import com.example.pizzeriaappdto.modulo.ClienteDTO
 import com.example.pizzeriaappdto.modulo.EstadoPedido
@@ -78,7 +79,22 @@ fun main() {
             6.0,
             SIZE.GRANDE,
             listOf(Ingrediente(1, "Gamba", listOf("Glutoma")),Ingrediente(1, "Kebab", listOf("Cancer", "Gluten")),Ingrediente(1, "Kebab", listOf("Cancer", "Gluten")))
+        ),
+        PizzaDTO(
+            4,
+            "Vegana",
+            6.0,
+            SIZE.GRANDE,
+            listOf(Ingrediente(1, "Cebolla", listOf("Glutoma")))
+        ),
+        PizzaDTO(
+            4,
+            "Vegana",
+            6.0,
+            SIZE.GRANDE,
+            listOf(Ingrediente(1, "Pimiento", listOf("Glutoma")))
         )
+
     )
 
     //Cliente no puede comer ingreditnes con estos alergenos:
@@ -128,6 +144,35 @@ fun main() {
     println(control.contarPizzasPorIngredienteDos(listaingr[0], listaPizzas))
     println(control.contarPizzasPorIngredienteDos(listaingr[1], listaPizzas))
     println(control.contarPizzasPorIngredienteDos(listaingr[2], listaPizzas))
+
+    //Devolvemos pizzas pa:
+    println(control.filtradoPorTamanyo(listaPizzas,SIZE.GRANDE))
+    println()
+    //Devolvemos pizzas por nombre:
+    println(control.filtradoPorNombreDeLaPizza(listaPizzas,"Pizza Kebab"))
+    println()
+    //Devolvemos pizzas con ingredientes contados:
+    println(control.filtradoPorNumeroDeIngredientes(listaPizzas,2))
+    println()
+    println(control.filtradorPrecioPromedio(listaPizzas))
+
+    val listaVeganos:List<Ingrediente> = listOf(Ingrediente(2,"Zanahoria", listOf()),Ingrediente(2,"Cebolla", listOf()),Ingrediente(2,"Pimiento", listOf()))
+
+    println()
+    println(control.filtrarPizzasVeganas(listaPizzas,listaVeganos))
+
+
+    println()
+    println(control.filtradorPrecioMasAltoQuePromedio(listaPizzas))
+
+
+    println()
+    println(control.filtradoPizzaConMasIngredientes(listaPizzas))
+
+
+
+
+
 
 }
 
@@ -180,4 +225,51 @@ class Controlador {
      */
     // listaPizzasAFiltrar.filter {it.listaIngredientesPizza.all { it.nombre.equals(ingre) }}.count()
     //return listaPizzasAFiltrar.filter {it.listaIngredientesPizza.all { it.nombre.equals(ingre) }}.count()
+
+
+    /**
+     * Funciones de prueba:
+     */
+
+    //Filtrado por tamanyo
+    fun filtradoPorTamanyo(listaPizas:List<PizzaDTO>,tamanyo:SIZE):List<PizzaDTO>{
+        return listaPizas.filter { it.tamanyo.equals(tamanyo) }
+    }
+
+    //Búsqueda de pizzas por nombre parcial: Dado un fragmento de texto, buscar todas las pizzas cuyos nombres contengan dicho fragmento.
+    fun filtradoPorNombreDeLaPizza(listaPizas:List<PizzaDTO>,nombre:String):List<PizzaDTO>{
+        return listaPizas.filter { it.nombre.equals(nombre,ignoreCase = true) }
+    }
+
+    //Filtrado por número de ingredientes: Filtrar las pizzas que tengan un número de ingredientes superior o inferior a un valor pasado por parámetro.
+    fun filtradoPorNumeroDeIngredientes(listaPizzas:List<PizzaDTO>, numeroDeIngredientes:Int):List<PizzaDTO>{
+        return listaPizzas.filter { it.listaIngredientesPizza.size>numeroDeIngredientes }
+    }
+
+    //Promedio de precio de pizzas: Calcular el precio promedio de todas las pizzas en la lista.
+    fun filtradorPrecioPromedio(listaPizzas:List<PizzaDTO>):Double{
+        return listaPizzas.sumOf { it.precio }/listaPizzas.size
+    }
+
+    //Identificación de pizzas veganas: Dado un listado de ingredientes, identificar las pizzas que solo contienen ingredientes veganos
+    fun filtrarPizzasVeganas(listaPizzas:List<PizzaDTO>,listaVeganas:List<Ingrediente>):List<PizzaDTO>{
+        return listaPizzas.filter { it.listaIngredientesPizza.all { it.nombre in listaVeganas.map { it.nombre }} }
+    }
+
+    //Agrupación de pizzas por ingredientes comunes: Agrupar las pizzas por la cantidad de ingredientes que tienen en común.
+
+
+
+    //Pizzas más caras que el promedio: Encontrar las pizzas que cuesten más que el precio promedio de la lista.
+    fun filtradorPrecioMasAltoQuePromedio(listaPizzas:List<PizzaDTO>):List<PizzaDTO>{
+        return listaPizzas.filter { it.precio > (listaPizzas.sumOf { it.precio }/listaPizzas.size) }
+    }
+
+//return listaPizzas.sumOf { it.precio }/listaPizzas.size
+//return listaPizzas.filter { it.precio > filtradorPrecioPromedio(listaPizzas) }
+
+    //Pizza con mayor número de ingredientes: Encontrar la pizza que tenga la mayor cantidad de ingredientes.
+    fun filtradoPizzaConMasIngredientes(listaPizzas:List<PizzaDTO>):PizzaDTO?{
+        return listaPizzas.maxByOrNull { it.listaIngredientesPizza.size }
+    }
 }
