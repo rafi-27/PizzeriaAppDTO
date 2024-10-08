307ff5d84e7b2033ba3f94ca355b5eb0921df898
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-10-08 10:18:08 +0200

    ya

--- app/src/main/java/com/example/pizzeriaappdto/modelo/mainKebab.kt
+++ app/src/main/java/com/example/pizzeriaappdto/modelo/mainKebab.kt
@@ -1,10 +1,12 @@
 package com.example.pizzeriaappdto.modelo
+import kotlin.collections.listOf
 
 fun main() {
     var listaKebabs: List<KebabDTO> = listOf(
         KebabDTO(listOf(Ingrediente(1,"PrimerKebab", listOf())),SIZE.PEQUEÑA,TIPO_CARNE.POLLO,10.0),
         KebabDTO(listOf(Ingrediente(2,"CuartoKebab", listOf())),SIZE.GRANDE,TIPO_CARNE.MIXTO,12.0),
         KebabDTO(listOf(Ingrediente(3,"TercerKebab", listOf())),SIZE.MEDIANA,TIPO_CARNE.TERNERA,18.0))
+
 
 
 
@@ -16,33 +18,32 @@ fun main() {
 
     //println(listaKebabs.filter { it.tipoCarne==TIPO_CARNE.POLLO }.forEach())
 
+    listaKebabs.forEach{println(it.toString())}
 
-
-    println(listaKebabs.forEach{it})
-
-    println(listaKebabs.map { it.precio }.forEach({it:KebabDTO-> println(it) }))
-
-
+    listaKebabs.map { it.precio*2 }.forEach{ println() }
+    //listaKebabs.forEach{it}
 
 
 
 
 
-    //Sin lambda:
-    for (kebab in listaKebabs){
-        println(kebab.toString()+"\\n")
-    }
-    println()
 
-    for (kebab in listaKebabs){
-        println(kebab.precio*2)
-    }
-    println()
 
-    for (keba in listaKebabs){
-        if (keba.tipoCarne==TIPO_CARNE.POLLO){
-            println(keba)
-        }
-    }
+//    //Sin lambda:
+//    for (kebab in listaKebabs){
+//        println(kebab.toString()+"\\n")
+//    }
+//    println()
+//
+//    for (kebab in listaKebabs){
+//        println(kebab.precio*2)
+//    }
+//    println()
+//
+//    for (keba in listaKebabs){
+//        if (keba.tipoCarne==TIPO_CARNE.POLLO){
+//            println(keba)
+//        }
+//    }
 
 }
